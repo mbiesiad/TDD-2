b3a34eab022eee43c3ccec18680316596638531b
Michal <mich.biesiada@gmail.com>
2020-02-06 14:18:36 +0100

    initial
    initial commit

---
+++ UnitTestProject/UnitTestProject/SimpleCalc.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTestProject
+{
+    class SimpleCalc
+    {
+        public double Add(double val1, double val2)
+        {
+            return val1 + val2;
+        }
+        public double Divide(double val1, double val2)
+        {
+            return val1 * val2; //intentionally wrong 1st
+        }
+    }
+}
+// fill this as a next step (2nd). use RGR
---
+++ UnitTestProject/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            SimpleCalc calc = new SimpleCalc();
+            double result = calc.Add(2, 3);
+            Assert.AreEqual(6, result); //intentionally wrong 1st
+        }
+        [TestMethod]
+        public void TestDivide()
+        {
+            SimpleCalc calc = new SimpleCalc();
+            double result = calc.Divide(100, 2);
+            Assert.AreEqual(50, result); //correct, but make wrong inside class
+        }
+    }
+}
+// now, fill this first.
+// simple example TDD for IT blog.
